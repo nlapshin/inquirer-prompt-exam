86292426e1569236f05ccc9b99f3c0375ebec74e
Nikolay Lapshin <nlapshin1989@gmail.com>
2018-10-17 21:36:43 +0300

    Added takeUntil to events.keypress

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Base = require('inquirer/lib/prompts/base');
 const observe = require('inquirer/lib/utils/events');
 
 const chalk = require('chalk');
+const { takeUntil } = require('rxjs/operators');
 
 class ExamPrompt extends Base {
     constructor(questions, rl, answers) {
@@ -16,26 +17,28 @@ class ExamPrompt extends Base {
 
     _run(cb) {
         this.done = cb;
-    
+
         var events = observe(this.rl);
         var submit = events.line;
-        
+
         var validation = this.handleSubmitEvents(submit);
         validation.success.forEach(this.onEnd.bind(this));
 
-        events.keypress.forEach(this.render.bind(this));
-        
+        events.keypress
+            .pipe(takeUntil(validation.success))
+            .forEach(this.render.bind(this));
+
         this.render();
-    
+
         return this;
     }
-    
+
     render() {
         let message = this.getQuestion();
 
         const isFinal = this.status === 'answered';
         const isSuccess = this.success === true;
-        
+
         if (isFinal) {
             let messageColor = isSuccess ? 'cyan' : 'red';
 
@@ -47,21 +50,21 @@ class ExamPrompt extends Base {
         } else {
             message += this.rl.line;
         }
-    
+
         this.screen.render(message);
     }
 
     checkResult(answer) {
         return answer === this.successAnswer;
     }
-    
+
     onEnd(state) {
         this.answer = state.value;
         this.success = this.checkResult(state.value);
         this.status = 'answered';
-    
+
         this.render();
-    
+
         this.screen.done();
         this.done(this.success);
     }
